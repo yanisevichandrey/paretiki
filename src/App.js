b8a0fd8bc0fb0e552cb2037f33e048032b805962
Andrey <andrey.yanisevich@gmail.com>
2018-10-03 17:06:06 +0300

    change get data and setState

--- src/App.js
+++ src/App.js
@@ -7,51 +7,55 @@ class App extends Component {
 
   state = {
     currencies: [
-      {id: 1, img: 'https://endotech.io/img/coinicon/BTC.png', nameCur: 'BTC', priceUsd: 6800, priceUah: 150000, priceRub: 340000},
-      {id: 2, img: 'https://endotech.io/img/coinicon/ETH.png', nameCur: 'ETH', priceUsd: 250, priceUah: 7400, priceRub: 16000},
-      {id: 3, img: 'https://endotech.io/img/coinicon/XRP.png', nameCur: 'XRP', priceUsd: 0.2500, priceUah: 7.0231, priceRub: 17.228}
+      { id: 1, img: 'https://endotech.io/img/coinicon/BTC.png', nameCur: 'BTC', priceUsd: 6800, priceUah: 150000, priceRub: 340000 },
+      { id: 2, img: 'https://endotech.io/img/coinicon/ETH.png', nameCur: 'ETH', priceUsd: 250, priceUah: 7400, priceRub: 16000 },
+      { id: 3, img: 'https://endotech.io/img/coinicon/XRP.png', nameCur: 'XRP', priceUsd: 0.2500, priceUah: 7.0231, priceRub: 17.228 }
     ],
-    currCrypta: 1,
-    cursPrivat: []
+    currCrypta: 1
   }
 
-  componentDidMount(){
-    this.getPrivatCurs();
+  async componentDidMount() {
+    const data = await this.getPrivatCurs();
+    this.changeStateCurr(data);
   }
 
   getPrivatCurs = () => {
     return fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
       .then(response => {
         return response.json();
-      }).then( data => {
-        this.setState({cursPrivat: data})
-        this.setState({
-          currencies: [
-            {id: 1, img: 'https://endotech.io/img/coinicon/BTC.png', nameCur: 'BTC', priceUsd: 6800, priceUah: 6800 * this.state.cursPrivat[0].buy, priceRub: 6800 / this.state.cursPrivat[2].buy * this.state.cursPrivat[0].buy },
-            {id: 2, img: 'https://endotech.io/img/coinicon/ETH.png', nameCur: 'ETH', priceUsd: 250, priceUah: 250 * this.state.cursPrivat[0].buy, priceRub: 250 / this.state.cursPrivat[2].buy * this.state.cursPrivat[0].buy},
-            {id: 3, img: 'https://endotech.io/img/coinicon/XRP.png', nameCur: 'XRP', priceUsd: 0.2500, priceUah: 0.25 * this.state.cursPrivat[0].buy, priceRub: 0.25 / this.state.cursPrivat[2].buy * this.state.cursPrivat[0].buy}
-          ]
-        })
-        console.log(this.state.cursPrivat)
-      });
+      })
   }
 
-  handlerCurr = (index) =>{
-    this.setState({currCrypta: index});
+  changeStateCurr = (data) => {
+    this.setState((prevState) => {
+      console.log('------', prevState.currencies);
+      console.log('------', data);
+      return {
+        currencies: prevState.currencies.map(cur => ({
+          ...cur,
+          priceUah: cur.priceUsd * data[0].buy,
+          priceRub: cur.priceUsd / data[2].buy * data[0].buy
 
+        }))
+      }
+    })
+  }
+
+  handlerCurr = (index) => {
+    this.setState({ currCrypta: index });
   }
 
   render() {
 
     const currsList = this.state.currencies.map((curr, index) => {
-      return <Currency 
+      return <Currency
         key={curr.id}
         nameCur={curr.nameCur}
         priceUsd={curr.priceUsd}
         priceUah={curr.priceUah}
         priceRub={curr.priceRub}
         img={curr.img}
-        clicked={() => this.handlerCurr(index)}/>
+        clicked={() => this.handlerCurr(index)} />
     })
 
     return (
@@ -59,11 +63,11 @@ class App extends Component {
         <div className="flex_between">
           {currsList}
         </div>
-        <InputComponent 
-        nameCur={this.state.currencies[this.state.currCrypta].nameCur}
-        currencies={this.state.currencies}
-        currCrypta={this.state.currCrypta}
-        cursPrivat={this.state.cursPrivat}/>
+        <InputComponent
+          nameCur={this.state.currencies[this.state.currCrypta].nameCur}
+          currencies={this.state.currencies}
+          currCrypta={this.state.currCrypta}
+          cursPrivat={this.state.cursPrivat} />
       </div>
     );
   }
